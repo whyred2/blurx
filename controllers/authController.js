34b1b6bc0079c86cff88708ab9c0b55f9934aa99
whyred <dima74181@gmail.com>
2024-06-02 15:32:53 +0300

    remove bcrypt and switch to bcryptjs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,4 @@
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const userModel = require('../modules/userModule');
 const AWS = require('aws-sdk');
